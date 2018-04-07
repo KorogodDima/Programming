92632dabecf68ea812b731986408d5978d97f4be
KorogodDima <KorogodDima@users.noreply.github.com>
2018-04-07 22:22:31 +0500

    RepairService

---
+++ RepairService/Servise/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servise
+{
+    public class RepairRequest
+    {
+        /// <summary>
+        /// Дата заполнения
+        /// </summary>
+        public DateTime Filled { get; set; }
+        /// <summary>
+        /// ФИО пассажира
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Модель
+        /// </summary>
+        public string Model { get; set; }
+        /// <summary>
+        /// Стоимость
+        /// </summary>
+        public decimal Price { get; set; }
+    }
+    public enum Currency
+    {
+        Rubles
+    }
+    public class FaultType
+    {
+        /// <summary>
+        /// Описание неисправности
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Решение
+        /// </summary>
+        public string Solution { get; set; }
+    }
+    public enum DeviceType
+    {
+        Smartphone,
+        Tablet,
+        Laptop,
+        PC,
+    }
+}
